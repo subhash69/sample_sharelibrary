6326819154724f1eabd446be79ef2f4fba437178
subhash <52318051+subhash69@users.noreply.github.com>
2022-03-30 14:56:44 +0530

    Update cal.groovy

--- vars/cal.groovy
+++ vars/cal.groovy
@@ -1,6 +1,6 @@
 def add(x,y){
   echo "sum of ${x} and ${y} is ${x+y}"
 }
-def mul(x,y){
-  echo "multiplication of ${x} and ${y} is ${x*y}"
+def mul(x,y,z){
+  echo "multiplication of ${x} ${y} ${z} is ${x*y*z}"
 }
